024df628d089be28b04d02c547eab8b37f41259d
Samuel Mercado <samuel.mg@protonmail.com>
2023-10-19 16:04:03 -0600

    WIP: layout

--- resources/views/components/mi-layout.blade.php
+++ resources/views/components/mi-layout.blade.php
@@ -8,16 +8,16 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Star Admin2 </title>
   <!-- plugins:css -->
-  <link rel="stylesheet" href="vendors/feather/feather.css">
-  <link rel="stylesheet" href="vendors/mdi/css/materialdesignicons.min.css">
-  <link rel="stylesheet" href="vendors/ti-icons/css/themify-icons.css">
-  <link rel="stylesheet" href="vendors/typicons/typicons.css">
-  <link rel="stylesheet" href="vendors/simple-line-icons/css/simple-line-icons.css">
-  <link rel="stylesheet" href="vendors/css/vendor.bundle.base.css">
+  <link rel="stylesheet" href="{{ asset('star/vendors/feather/feather.css') }}">
+  <link rel="stylesheet" href="{{ asset('star/vendors/mdi/css/materialdesignicons.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('star/vendors/ti-icons/css/themify-icons.css') }}">
+  <link rel="stylesheet" href="{{ asset('star/vendors/typicons/typicons.css') }}">
+  <link rel="stylesheet" href="{{ asset('star/vendors/simple-line-icons/css/simple-line-icons.css') }}">
+  <link rel="stylesheet" href="{{ asset('star/vendors/css/vendor.bundle.base.css') }}">
   <!-- endinject -->
   <!-- Plugin css for this page -->
-  <link rel="stylesheet" href="vendors/datatables.net-bs4/dataTables.bootstrap4.css">
-  <link rel="stylesheet" href="js/select.dataTables.min.css">
+  <link rel="stylesheet" href="{{ asset('star/vendors/datatables.net-bs4/dataTables.bootstrap4.css') }}">
+  <link rel="stylesheet" href="{{ asset('star/js/select.dataTables.min.css') }}">
   <!-- End plugin css for this page -->
   <!-- inject:css -->
   <link rel="stylesheet" href="{{ asset('star/css/vertical-layout-light/style.css') }}">
@@ -505,25 +505,25 @@
   <!-- container-scroller -->
 
   <!-- plugins:js -->
-  <script src="vendors/js/vendor.bundle.base.js"></script>
+  <script src="{{ asset('star/vendors/js/vendor.bundle.base.js') }}"></script>
   <!-- endinject -->
   <!-- Plugin js for this page -->
-  <script src="vendors/chart.js/Chart.min.js"></script>
-  <script src="vendors/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
-  <script src="vendors/progressbar.js/progressbar.min.js"></script>
+  <script src="{{ asset('star/vendors/chart.js/Chart.min.js') }}"></script>
+  <script src="{{ asset('star/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js') }}"></script>
+  <script src="{{ asset('star/vendors/progressbar.js/progressbar.min.js') }}"></script>
 
   <!-- End plugin js for this page -->
   <!-- inject:js -->
-  <script src="js/off-canvas.js"></script>
-  <script src="js/hoverable-collapse.js"></script>
-  <script src="js/template.js"></script>
-  <script src="js/settings.js"></script>
-  <script src="js/todolist.js"></script>
+  <script src="{{ asset('star/js/off-canvas.js') }}"></script>
+  <script src="{{ asset('star/js/hoverable-collapse.js') }}"></script>
+  <script src="{{ asset('star/js/template.js') }}"></script>
+  <script src="{{ asset('star/js/settings.js') }}"></script>
+  <script src="{{ asset('star/js/todolist.js') }}"></script>
   <!-- endinject -->
   <!-- Custom js for this page-->
-  <script src="js/jquery.cookie.js" type="text/javascript"></script>
-  <script src="js/dashboard.js"></script>
-  <script src="js/Chart.roundedBarCharts.js"></script>
+  <script src="{{ asset('star/js/jquery.cookie.js" type="text/javascript') }}"></script>
+  <script src="{{ asset('star/js/dashboard.js') }}"></script>
+  <script src="{{ asset('star/js/Chart.roundedBarCharts.js') }}"></script>
   <!-- End custom js for this page-->
 </body>
 
--- resources/views/normas/norma-index.blade.php
+++ resources/views/normas/norma-index.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Listado Normas</title>
-</head>
-<body>
-    <h1>Listado de Normas</h1>
+<x-mi-layout>
+<h1>Listado de Normas</h1>
 
     <ul>
         @foreach ($normas as $norma)
@@ -29,5 +21,4 @@
             </li>
         @endforeach
     </ul>
-</body>
-</html>
+</x-mi-layout>
